0b9ccc5462028d7b0facde66b230465d47969ceb
shashishsoni <shshshsoni2003@tutanota.com>
2024-12-28 02:29:12 +0530

    12/28

--- client/home-rental-booking/src/components/listings.tsx
+++ client/home-rental-booking/src/components/listings.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { categories } from "../data";
 import ListingCard from "./listingcard";
 import Loader from "./loader";
