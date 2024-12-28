016c3324309dbe7435e12c2d34b55d0687469edb
shashishsoni <shshshsoni2003@tutanota.com>
2024-12-29 04:10:23 +0530

    update

--- client/home-rental-booking/src/main.tsx
+++ client/home-rental-booking/src/main.tsx
@@ -1,22 +1,18 @@
 // src/main.tsx
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './redux/storecache';
 import App from './App';
 import "./index.css";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-
-root.render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <App />
       </PersistGate>
     </Provider>
-  </React.StrictMode>
+  </StrictMode>
 );
--- client/home-rental-booking/src/redux/storecache.ts
+++ client/home-rental-booking/src/redux/storecache.ts
@@ -1,6 +1,6 @@
 // storecache.ts
 import { configureStore } from "@reduxjs/toolkit";
-import { persistStore, persistReducer } from "redux-persist";
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import { combineReducers } from '@reduxjs/toolkit';
 import userReducer from "./cache";
@@ -13,6 +13,7 @@ const rootReducer = combineReducers({
 
 const persistConfig = {
   key: "root",
+  version: 1,
   storage,
   whitelist: ["user"]
 };
@@ -23,8 +24,11 @@ export const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-      serializableCheck: false
-    })
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+  devTools: process.env.NODE_ENV !== 'production'
 });
 
 export const persistor = persistStore(store);
