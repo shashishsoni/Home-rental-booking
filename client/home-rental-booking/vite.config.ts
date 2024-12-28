f74e778221d92ed66ddec8c1de749190ed5cb36f
shashishsoni <shshshsoni2003@tutanota.com>
2024-12-28 15:00:29 +0530

    update

--- client/home-rental-booking/vite.config.ts
+++ client/home-rental-booking/vite.config.ts
@@ -1,41 +1,68 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
-import type { UserConfig } from 'vite'
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from "vite";
+import viteImagemin from "vite-plugin-imagemin";
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  build: {
-    reportCompressedSize: false,
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true
-      }
-    },
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html')
-      },
-      output: {
-        manualChunks: undefined,
-        assetFileNames: (info) => {
-          if (!info.name) return 'assets/[name].[hash][extname]';
-          
-          const extType = info.name.split('.').pop();
-          if (extType && /png|jpe?g|svg|gif|tiff|bmp|ico|webp|avif/i.test(extType)) {
-            return `assets/images/[name].[hash][extname]`;
-          }
-          return `assets/[name].[hash][extname]`;
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [
+      react(),
+      viteImagemin({
+        gifsicle: { optimizationLevel: 7 },
+        optipng: { optimizationLevel: 7 },
+        mozjpeg: { quality: 80 },
+        svgo: {
+          plugins: [
+            { name: "removeViewBox" },
+            { name: "removeEmptyAttrs", active: false },
+          ],
         },
-      }
-    }
-  },
-  logLevel: 'error'
-} as UserConfig)
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    server: {
+      host: "0.0.0.0",
+      port: 3000,
+    },
+    build: {
+      reportCompressedSize: false,
+      minify: "terser",
+      terserOptions: {
+        compress: {
+          drop_console: true,
+          drop_debugger: true,
+        },
+      },
+      rollupOptions: {
+        input: {
+          main: path.resolve(__dirname, "index.html"),
+        },
+        output: {
+          manualChunks(id) {
+            if (id.includes("node_modules")) {
+              return id.split("node_modules/")[1].split("/")[0].toString();
+            }
+          },
+          assetFileNames: (info) => {
+            if (!info.name) return "assets/[name].[hash][extname]";
+            const extType = info.name.split(".").pop();
+            if (extType && /png|jpe?g|svg|gif|tiff|bmp|ico|webp|avif/i.test(extType)) {
+              return `assets/images/[name].[hash][extname]`;
+            }
+            return `assets/[name].[hash][extname]`;
+          },
+        },
+      },
+    },
+    define: {
+      "process.env": env,
+    },
+    logLevel: "error",
+  };
+});
