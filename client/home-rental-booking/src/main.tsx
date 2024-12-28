770c5f66f0c937eb39c76aa339055fd9093532e2
shashishsoni <shshshsoni2003@tutanota.com>
2024-12-29 04:23:43 +0530

    update

--- client/home-rental-booking/src/main.tsx
+++ client/home-rental-booking/src/main.tsx
@@ -1,5 +1,4 @@
 // src/main.tsx
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -8,16 +7,12 @@ import App from './App';
 import "./index.css";
 
 const container = document.getElementById('root');
-if (!container) throw new Error('Failed to find the root element');
+if (!container) throw new Error('Root element not found');
 
-const root = ReactDOM.createRoot(container);
-
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <App />
-      </PersistGate>
-    </Provider>
-  </React.StrictMode>
+ReactDOM.createRoot(container).render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
 );
--- client/home-rental-booking/src/redux/storecache.ts
+++ client/home-rental-booking/src/redux/storecache.ts
@@ -6,26 +6,33 @@ import { combineReducers } from '@reduxjs/toolkit';
 import userReducer from "./cache";
 import { searchReducer } from './cache';
 
+// Create root reducer
 const rootReducer = combineReducers({
   user: userReducer,
   search: searchReducer
 });
 
+// Basic persist config
 const persistConfig = {
   key: "root",
-  storage
+  storage,
+  blacklist: [] // Don't persist any reducers
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-export const store = configureStore({
+// Create store with basic config
+const store = configureStore({
   reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) =>
+  middleware: (getDefaultMiddleware) => 
     getDefaultMiddleware({
-      serializableCheck: false
+      serializableCheck: false,
+      immutableCheck: false
     })
 });
 
-export const persistor = persistStore(store);
+const persistor = persistStore(store);
+
+export { store, persistor };
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
