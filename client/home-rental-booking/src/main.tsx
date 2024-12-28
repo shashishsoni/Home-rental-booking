eee322ef64ccddd2383fbc0becab08f9e3a2855c
shashishsoni <shshshsoni2003@tutanota.com>
2024-12-29 03:28:28 +0530

    update

--- client/home-rental-booking/src/main.tsx
+++ client/home-rental-booking/src/main.tsx
@@ -1,22 +1,18 @@
 // src/main.tsx
-import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import { store, getPersistor } from "./redux/storecache";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/storecache';
+import App from './App';
 import "./index.css";
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error('Failed to find root element');
-}
-
-const root = ReactDOM.createRoot(rootElement);
-
-root.render(
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={getPersistor()}>
-      <App />
-    </PersistGate>
-  </Provider>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
 );
--- client/home-rental-booking/src/redux/storecache.ts
+++ client/home-rental-booking/src/redux/storecache.ts
@@ -1,103 +1,23 @@
 // storecache.ts
 import { configureStore } from "@reduxjs/toolkit";
-import { UserState } from "../types/types";
-import userReducer from "./cache";
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-  PersistedState,
-  PersistConfig,
-  PersistState
-} from "redux-persist";
+import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-import { combineReducers } from "redux";
-import { searchReducer } from './cache';
-import { SearchState } from './cache';
+import rootReducer from "./cache";
 
-// Define the root state type
-export interface RootState {
-  user: UserState;
-  search: SearchState;
-}
-
-// Define our custom state structure
-interface CustomState {
-  user: {
-    user: UserState["user"];
-    token: string | null;
-    profileImagePath: string | null;
-  };
-  _persist: PersistState;
-}
-
-// Combine reducers
-const rootReducer = combineReducers({
-  user: userReducer,
-  search: searchReducer
-});
-
-
-// Type-safe migration functions
-type MigrationFunction = (state: PersistedState) => Promise<PersistedState>;
-
-const migrations: Record<number, MigrationFunction> = {
-  0: async (_: PersistedState): Promise<PersistedState> => {
-    return {
-      user: {
-        user: null,
-        token: null,
-        profileImagePath: null,
-      },
-      _persist: { version: 0, rehydrated: true }
-    } as PersistedState;
-  },
-  1: async (state: PersistedState): Promise<PersistedState> => {
-    const currentState = state as CustomState;
-    return {
-      user: {
-        user: currentState?.user?.user || null,
-        token: currentState?.user?.token || null,
-        profileImagePath: currentState?.user?.profileImagePath || null,
-      },
-      _persist: { version: 1, rehydrated: true }
-    } as PersistedState;
-  }
-};
-
-// Persist configuration
-const persistConfig: PersistConfig<RootState> = {
+const persistConfig = {
   key: "root",
-  version: 1,
   storage,
-  whitelist: ["user"],
-  migrate: async (state: PersistedState): Promise<PersistedState> => {
-    if (!state) return migrations[0](state);
-    const version = (state as CustomState)?._persist?.version || 0;
-    const migrate = migrations[version];
-    return migrate ? await migrate(state) : migrations[0](state);
-  },
+  whitelist: ["user"]
 };
 
-// Create persisted reducer
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-// Create Redux store
 export const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
+      serializableCheck: false
+    })
 });
 
-const persistor = persistStore(store);
-export const getStore = () => store;
-export const getPersistor = () => persistor;
+export const persistor = persistStore(store);
