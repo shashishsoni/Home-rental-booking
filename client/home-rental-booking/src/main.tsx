8fc96b0b8b65b2bf71d14e77c7f1fac3ad01167d
shashishsoni <shshshsoni2003@tutanota.com>
2024-12-29 03:58:33 +0530

    update

--- client/home-rental-booking/src/main.tsx
+++ client/home-rental-booking/src/main.tsx
@@ -1,5 +1,4 @@
 // src/main.tsx
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -12,11 +11,9 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
 );
--- client/home-rental-booking/src/redux/storecache.ts
+++ client/home-rental-booking/src/redux/storecache.ts
@@ -1,55 +1,33 @@
 // storecache.ts
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from "redux-persist";
+import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import { combineReducers } from "redux";
 import userReducer from "./cache";
 import { searchReducer } from './cache';
 
-// Create root reducer
 const rootReducer = combineReducers({
   user: userReducer,
   search: searchReducer
 });
 
-// Configure persist
 const persistConfig = {
   key: "root",
-  version: 1,
   storage,
-  whitelist: ["user"],
-  blacklist: ["_persist"]
+  whitelist: ["user"]
 };
 
-// Create persisted reducer
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-// Configure store
 export const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-        ignoredPaths: ['_persist'],
-      },
-      immutableCheck: false,
-    }),
-  devTools: process.env.NODE_ENV !== 'production',
+      serializableCheck: false,
+      immutableCheck: false
+    })
 });
 
-// Create persistor
 export const persistor = persistStore(store);
-
-// Export types
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
