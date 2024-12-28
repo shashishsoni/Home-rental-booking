a65f1a409176834a1f4ecae3346b53a12cf8c373
shashishsoni <shshshsoni2003@tutanota.com>
2024-12-29 03:54:01 +0530

    update

--- client/home-rental-booking/src/redux/storecache.ts
+++ client/home-rental-booking/src/redux/storecache.ts
@@ -1,35 +1,55 @@
 // storecache.ts
-import { configureStore } from "@reduxjs/toolkit";
-import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import { combineReducers } from "redux";
 import userReducer from "./cache";
 import { searchReducer } from './cache';
 
+// Create root reducer
 const rootReducer = combineReducers({
   user: userReducer,
   search: searchReducer
 });
 
+// Configure persist
 const persistConfig = {
   key: "root",
   version: 1,
   storage,
-  whitelist: ["user"]
+  whitelist: ["user"],
+  blacklist: ["_persist"]
 };
 
+// Create persisted reducer
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
+// Configure store
 export const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        ignoredPaths: ['_persist'],
       },
+      immutableCheck: false,
     }),
+  devTools: process.env.NODE_ENV !== 'production',
 });
 
+// Create persistor
 export const persistor = persistStore(store);
+
+// Export types
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
